1433850dde4e8dd31d9f0f5faf231d75e82b5829
Hassan <ijeyleekrane@gmail.com>
2025-03-06 18:03:46 +0100

    Second Changess madee to utils contants colors file

---
+++ lib/src/utils/contants/colors.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+// Main Application Theme Colors
+//---------------------------------
+//primary color
+const firstPrimaryColor = Color(0xff21790e);
+//secondary color
+const firstSecondaryColor = Color(0xff27270e) ;
+//backup color
+const hAccentColor = Color(0xff001bff);
+
+
+//Text colors
+//---------------------------------
+//Primary Text color
+const Color hTextPrimaryColor = Color(0xff333333);
+//Secondary Text Color
+const Color hTextSecondaryColor = Color(0xff6c757d);
+//Text White Color
+const Color hTextWhiteColor = Color(0xfff6f6f6);
+
+
+//Background Colors
+//----------------------------------
+//Background Light Color
+const  bLightColor = Color(0xfff6f6f6);
+//Background Dark Color
+const  bDarkColor = Color(0xff272722);
+//Primary Background color
+const  bPrimaryBackgroundColor = Color(0xfff3f5ff);
+
+
+//Background Container Color
+//---------------------------------
+//Light Container Color
+const bLightContainerColor = Color(0xfff6f6f6);
+//Dark Container Color
+Color bDarkContainerColor = Colors.white.withOpacity(0.1);
+
+
+//Button Color
+//--------------------------------
+//primary button color
+const Color hButtonPrimaryColor= Color(0xff4b68ff);
+//secondary button color
+const Color hButtonSecondaryColor = Color(0xff6c757d);
+//disabled color
+const Color dButtonDisabledColor  = Color(0xffc4c4c4);
+
+
+//Error Colors.
+//---------------------------------
+//When Transaction is successful
+const Color hSuccessColor = Color(0xff21790e);
+//when transaction is failed or error
+const Color hErrorColor = Color(0xffFF5252);
+//when transaction is pending
+const Color hPendingColor = Color(0xffFFC107);
+//in progress color
+const Color hInProgressColor = Color(0xff43A0A7);
+
+
+//Border Colors
+//---------------------------------
+//Border Primary Color
+const Color hBorderPrimaryColor = Color(0xfff6f6f6);
+//Border Secondary Color
+const Color hBorderSecondaryColor = Color(0xff6c757d);
+
+
+//General Colors
+//---------------------------------
+//Black Color
+const Color hGeneralColor = Color(0xff232323);
+//Dark Grey Color
+const Color hDarkGreyColor = Color(0xff6c757d);
+//Light Grey Color
+const Color hLightGreyColor = Color(0xfff6f6f6);
+//grey Color
+const Color hGreyColor = Color(0xff6c757d);
+//white Color
+const Color hWhiteColor = Color(0xfff6f6f6);
+
+
+
+//Card Background Color
+//---------------------------------
+const Color hCardBackgroundColor = Color(0xfff6f6f6);
+//Pitch dark color
+const Color hPitchDarkColor = Color(0xff000000);
+//Pitch light color
+const Color hPitchLightColor = Color(0xfff6f6f6);
